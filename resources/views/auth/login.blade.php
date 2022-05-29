3cb65cfa06af2aac5b2afa01597237a145046c58
unknown <i.mihai9960@gmail.com>
2022-05-29 15:15:15 +0300

    Added login and logout functionality

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <form action="{{ route('register') }}" method="POST">
+    <form action="{{ route('login') }}" method="POST">
         @csrf
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input id="name" name="name" value="{{ old('name') }}" type="text" required
-                class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" />
-
-            @if ($errors->has('name'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('name') }}</strong>
-                </span>
-            @endif
-        </div>
-
         <div class="form-group">
             <label for="email">E-mail</label>
             <input id="email" name="email" value="{{ old('email') }}" type="email" required
@@ -39,12 +27,16 @@
         </div>
 
         <div class="form-group">
-            <label for="password_confirmation">Retyped Password</label>
-            <input id="password_confirmation" name="password_confirmation" type="password" class="form-control" />
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember"
+                    value="{{ old('remember') ? 'checked' : '' }}" />
+
+                <label class="form-check-label" for="remember">Remember Me</label>
+            </div>
         </div>
 
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Register" />
+            <input type="submit" class="btn btn-primary value=" Login" />
         </div>
     </form>
 @endsection
