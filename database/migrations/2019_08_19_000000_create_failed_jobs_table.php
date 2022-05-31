fd1326e0e48d445993be77baed8f33a9fb961091
unknown <i.mihai9960@gmail.com>
2022-05-31 21:27:12 +0300

    Added foreign key on blog_posts table referencing users table

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -6,31 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateFailedJobsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('failed_jobs', function (Blueprint $table) {
+      $table->id();
+      $table->string('uuid')->unique();
+      if (env('DB_CONNECTION') === 'sqlite_testing') {
+        $table->text('connection')->default('');
+        $table->text('queue')->default('');
+        $table->longText('payload')->default('');
+        $table->longText('exception')->default('');
+      } else {
+        $table->text('connection');
+        $table->text('queue');
+        $table->longText('payload');
+        $table->longText('exception');
+      }
+      $table->timestamp('failed_at')->useCurrent();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('failed_jobs');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('failed_jobs');
+  }
 }
